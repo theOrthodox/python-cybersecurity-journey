25af6df107a380bdbe5d8e12e77a65c2e25e7dda
Vinit Kumar Singh <svinit684@gmail.com>
2026-06-29 13:31:05 +0530

    Update ipv4_validatory.py

--- 01-ipv4-validator/ipv4_validatory.py
+++ 01-ipv4-validator/ipv4_validatory.py
@@ -2,31 +2,31 @@
 PROJECT : IPv4 VALIDATOR
 Validates IPv4 addresses accoding to standard IPv4 formatting rules
 
-Authon : VINIT KUMAR SINGH
+Author : VINIT KUMAR SINGH
 '''
 ip_address = input("enter the ip address ")
 print(f"The IP address entered : {ip_address}")
 
 def validator(ip_address):
-    octects = list()
     octects=ip_address.split(".")
     valid=True
-    if len(octects) != 4 :
+    if len(octects) != 4 :  # to check if there are 4 octects only
         valid=False
     for num in octects :
       try:
+          if num == '':  # to check if it has a blank octect
+             valid=False
+             break
           num=int(num) 
-          if num > 255 or num < 0:
+          if num > 255 or num < 0: # to check if it is in between 0 to 255
             valid=False
             break
-      except :
-         valid=False
+      except ValueError :
+         print("Entered value out of range")
+         return False
        
-    if valid == True :
-       return True
-    else :
-      return False
-      
+    return valid
+
 response =validator(ip_address)
 if response is True :
    print("IP entered is valid")

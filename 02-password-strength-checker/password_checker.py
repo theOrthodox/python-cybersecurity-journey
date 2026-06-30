2835b9fb25a61c0197aa53a92f489c93b324195f
Vinit Kumar Singh <svinit684@gmail.com>
2026-06-30 15:45:27 +0530

    Update password_checker.py

--- 02-password-strength-checker/password_checker.py
+++ 02-password-strength-checker/password_checker.py
@@ -1,1 +1,34 @@
+print("===== WELCOME TO PASSWORD STRENGTH CHECKER =====")
+password=input("Please enter your password \t")
 
+def password_checker(password):
+    has_length = False
+    has_upper = False
+    has_lower = False
+    has_digit = False
+    has_special = False
+    specl = ['!', '@', '#', '$', '%','^', '&', '*', '(' ,')', '_' ,'+', '-', '=', '?' ,'/', '.']
+
+    if len(password) >= 8 :
+        has_length = True
+
+    for word in password :
+        if word.isupper():
+            has_upper = True
+        if word.islower():
+            has_lower = True
+        if word.isdigit():
+            has_digit = True
+        if word in specl:
+            has_special = True
+
+    if has_digit and has_length and has_lower and has_special and has_upper :
+        return True
+    else :
+        return False
+
+print(f"the password entered is : {password}")
+if password_checker(password):
+    print("strong")
+else:
+    print("Weak") 
